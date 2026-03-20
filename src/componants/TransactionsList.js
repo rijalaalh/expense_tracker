8753e7f5dd62e820bb4b8ee28a80fe338be29005
sajed <tsajed03@gmail.com>
2026-03-20 15:11:40 +0100

    changed varaible balance to be dynamique

--- src/componants/TransactionsList.js
+++ src/componants/TransactionsList.js
@@ -2,11 +2,18 @@
 import { useContext, useState } from 'react';
 import { Addtransactioncontext } from '../context/context';
 export default function TransactionsList() {
-  const { list, setlist } = useContext(Addtransactioncontext);
+  const { list, setlist, income, Setincome, expense, setexpense } = useContext(
+    Addtransactioncontext,
+  );
   const [removingIndex, setRemovingIndex] = useState(null);
   function Delete(index) {
     setRemovingIndex(index);
-
+    const removingprice = JSON.parse(localStorage.getItem('list'));
+    if (Number(removingprice[index].price) < 0) {
+      setexpense(expense + Number(removingprice[index].price));
+    } else {
+      Setincome(income - Number(removingprice[index].price));
+    }
     setTimeout(() => {
       const updatedList = list.filter((_, i) => i !== index);
       setlist(updatedList);
